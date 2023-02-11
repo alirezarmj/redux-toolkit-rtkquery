cb40f19642e8c93a1b013480446ff8e5b9cf77b5
Rashidi-Moghadam <a.rashidi.m78@gmail.com>
2023-02-11 20:28:09 +0330

    edit server address

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
     reducerPath: "api",
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3500" }),
+    baseQuery: fetchBaseQuery({ baseUrl: "https://todoapi.rashidim.com" }),
     endpoints: (builder) => ({
         getTodos: builder.query({
             query: () => "todos",
